5764443a418ab5123a235a296e3eda45cdb8a827
zhaowb <zhaowb@gmail.com>
2017-06-09 07:12:55 +0000

    ubuntu

--- src-django/myproj/start_gunicorn.sh
+++ src-django/myproj/start_gunicorn.sh
@@ -1,1 +1,3 @@
-nohup gunicorn --bind 127.0.0.1:8003 myproj.wsgi &
+# nohup gunicorn --bind 127.0.0.1:8003 myproj.wsgi &
+# exec /home/zhaowb/test_site_1/env_site1/bin/gunicorn --pid /var/run/proj7427.pid --access-logfile - --bind 127.0.0.1:8003 myproj.wsgi
+exec /home/zhaowb/test_site_1/env_site1/bin/gunicorn --pid gunicorn.pid --access-logfile - --bind 127.0.0.1:8003 myproj.wsgi
